221692a52a5e7ada253ce806576824403ac18211
Grant Forrest <gaforres@gmail.com>
2019-11-10 15:32:27 -0500

    ...did it not work?

--- web/src/clouds/Scene.tsx
+++ web/src/clouds/Scene.tsx
@@ -7,6 +7,7 @@ import { Camera } from './Camera';
 import { CloudMap } from './CloudMap';
 import { Text } from './Text';
 import { Sun } from './Sun';
+import { NoSsr } from '@material-ui/core';
 
 export type SceneProps = {};
 
@@ -24,18 +25,20 @@ const textPosition = new Vector3(0, 0, -10);
 
 export const Scene: React.FC<SceneProps> = ({}) => {
   return (
-    <LightContext.Provider value={lightContext}>
-      <Canvas shadowMap={{ type: PCFSoftShadowMap }} style={{ backgroundColor: skyColor }}>
-        <React.Suspense fallback={null}>
-          <Camera position={cameraPosition} />
-          <Sun />
-          <ambientLight color={lightContext.ambientLightColor} />
-          <CloudMap velocity={windVelocity} />
-          <Text size={1} position={textPosition}>
-            Grant Forrest
-          </Text>
-        </React.Suspense>
-      </Canvas>
-    </LightContext.Provider>
+    <NoSsr>
+      <LightContext.Provider value={lightContext}>
+        <Canvas shadowMap={{ type: PCFSoftShadowMap }} style={{ backgroundColor: skyColor }}>
+          <React.Suspense fallback={null}>
+            <Camera position={cameraPosition} />
+            <Sun />
+            <ambientLight color={lightContext.ambientLightColor} />
+            <CloudMap velocity={windVelocity} />
+            <Text size={1} position={textPosition}>
+              Grant Forrest
+            </Text>
+          </React.Suspense>
+        </Canvas>
+      </LightContext.Provider>
+    </NoSsr>
   );
 };
